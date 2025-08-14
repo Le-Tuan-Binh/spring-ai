2d50126722efe2de02e3b6cbd9feebab503e8f4c
Le Tuan Binh <TBinTB0003@gmail.com>
2025-08-15 01:08:40 +0700

    Spring AI with Bedrock

---
+++ bedrock/src/test/java/tbin/bedrock/BedrockApplicationTests.java
@@ -0,0 +1,13 @@
+package tbin.bedrock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BedrockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
