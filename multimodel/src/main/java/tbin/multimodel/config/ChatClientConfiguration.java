e3130be1f95d28423ef7cffc26f9c8008bb7f864
Le Tuan Binh <TBinTB0003@gmail.com>
2025-08-26 00:51:23 +0700

    Spring AI Multi Model

---
+++ multimodel/src/main/java/tbin/multimodel/config/ChatClientConfiguration.java
@@ -0,0 +1,21 @@
+package tbin.multimodel.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfiguration {
+    @Bean
+    public ChatClient openAiChatClient(OpenAiChatModel  openAiChatModel) {
+        return ChatClient.create(openAiChatModel);
+    }
+
+    @Bean
+    public ChatClient ollamaChatClient(OllamaChatModel ollamaChatModel) {
+        ChatClient.Builder chatClientBuilder = ChatClient.builder(ollamaChatModel);
+        return chatClientBuilder.build();
+    }
+}
