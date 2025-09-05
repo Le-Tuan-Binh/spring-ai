571a35d081633bed9863b87372d21980377d9308
Le Tuan Binh <TBinTB0003@gmail.com>
2025-09-06 05:25:33 +0700

    Message role in Spring AI

---
+++ v2/springai/src/test/java/tbin/springai/SpringaiApplicationTests.java
@@ -0,0 +1,13 @@
+package tbin.springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
